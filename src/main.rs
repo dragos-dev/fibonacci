4572312b8e58e35e3b843025dc62976a5f0c825e
dragos-dev <dragos.tzx@gmail.com>
2022-12-19 04:47:04 +0300

    feat: add input and parse him

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use std::io;
+
 fn main() {
     loop {
+        let mut line = String::new();
+
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Invalid input");
+
+        let number: u128 = match line.parse() {
+            Ok(n) => if n > 1 { n } else {
+                println!("Enter a number greater than 1.");
+                continue
+            },
+            Err(err) => {
+                println!("Error: {}", err);
+                continue
+            }
+        };
 
     }
 }
